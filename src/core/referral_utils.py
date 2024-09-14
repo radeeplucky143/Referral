dcd75c9d1a215440ce5b5adcee4271f733a9422e
Radeep Yenni <radeeplucky143@gmail.com>
2024-09-14 17:15:02 +0530

    Logger functionalities modified.

--- src/core/referral_utils.py
+++ src/core/referral_utils.py
@@ -1,6 +1,7 @@
 from core.logger_utils import Logger
 import random
 import string
+import os
 
 def generate_referral_code(length=8):
     """Generates a 8 digit referral code consiting upper case alphabets and digits.
@@ -8,7 +9,8 @@ def generate_referral_code(length=8):
     Returns:
         A random string of uppercase letters and digits.
     """
-    logger = Logger(__name__).create_log_file()
+    parent_dir = os.path.dirname(os.getcwd())
+    logger = Logger(__name__, parent_dir).create_log_file()
     characters = string.ascii_uppercase + string.digits
     code = ''.join(random.choice(characters) for _ in range(length))
     logger.info(f"Referral code generated: {code}")
