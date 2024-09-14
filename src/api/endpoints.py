db9743b7eea31715e759cb4ca5df550bd5a836f6
Radeep Yenni <radeeplucky143@gmail.com>
2024-09-14 17:14:44 +0530

    generate_referral_code endpoint modified.

--- src/api/endpoints.py
+++ src/api/endpoints.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, HTTPException
+from core.referral_utils import generate_referral_code
 import asyncio
 
 router = APIRouter()
 
 @router.get("/generate_referral_code")
-async def generate_referral_code():
-    return "S7BHG9"
+async def referral_code_generation():
+    return generate_referral_code()
