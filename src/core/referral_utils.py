7fee441071fb3c46386697a03f84ed55e0e08276
Radeep Yenni <radeeplucky143@gmail.com>
2024-09-14 18:33:18 +0530

    config values loading in referral code generation function.

--- src/core/referral_utils.py
+++ src/core/referral_utils.py
@@ -1,4 +1,5 @@
 from core.logger_utils import Logger
+from core.yaml_utils import Yaml
 import random
 import string
 import os
@@ -9,8 +10,12 @@ def generate_referral_code(length=8):
     Returns:
         A random string of uppercase letters and digits.
     """
+
     parent_dir = os.path.dirname(os.path.dirname(__file__))
     logger = Logger(__name__, parent_dir).create_log_file()
+
+    config = Yaml().load_yaml()
+    logger.info("config file loaded successfully.")
     characters = string.ascii_uppercase + string.digits
     code = ''.join(random.choice(characters) for _ in range(length))
     logger.info(f"Referral code generated: {code}")
