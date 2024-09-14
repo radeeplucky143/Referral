74e7a18fd75652b93ff23466cbae5d84838fc22c
Radeep Yenni <radeeplucky143@gmail.com>
2024-09-14 21:48:07 +0530

    UserReferral BaseModel added.

--- src/api/models.py
+++ src/api/models.py
@@ -1,5 +1,10 @@
 from pydantic import BaseModel
 
 class MiningUser(BaseModel):
-    user_name: int
+    user_name: str
     email: str
+
+
+class UserReferral(BaseModel):
+    user_name: str
+    referral_code: str
