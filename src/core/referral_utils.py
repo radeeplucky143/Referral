eb3e102e272a6cec6977a9ced1c424f84d1bfb56
Radeep Yenni <radeeplucky143@gmail.com>
2024-09-14 21:45:32 +0530

    Referral Class designed.

--- src/core/referral_utils.py
+++ src/core/referral_utils.py
@@ -1,22 +1,35 @@
 from core.logger_utils import Logger
+from api.models import MiningUser
 from core.yaml_utils import Yaml
+from core.database_utils import Database
 import random
 import string
 import os
 
-def generate_referral_code(length=8):
-    """Generates a 8 digit referral code consiting upper case alphabets and digits.
 
-    Returns:
-        A random string of uppercase letters and digits.
-    """
+class Referral:
 
-    parent_dir = os.path.dirname(os.path.dirname(__file__))
-    logger = Logger(__name__, parent_dir).create_log_file()
+    def __init__(self):
+        self.parent_dir = os.path.dirname(os.path.dirname(__file__))
+        self.logger = Logger(__name__, self.parent_dir).create_log_file()
+        self.config = Yaml().load_yaml()
+        self.logger.info("config file loaded successfully.")
 
-    config = Yaml().load_yaml()
-    logger.info("config file loaded successfully.")
-    characters = string.ascii_uppercase + string.digits
-    code = ''.join(random.choice(characters) for _ in range(length))
-    logger.info(f"Referral code generated: {code}")
-    return code
+
+    def generate_referral_code(self, user: MiningUser, length=8):
+        """Generates a 8 digit referral code consiting upper case alphabets and digits.
+
+        Returns:
+            A random string of uppercase letters and digits.
+        """
+        characters = string.ascii_uppercase + string.digits
+        code = ''.join(random.choice(characters) for _ in range(length))
+
+        database = Database()
+        values = (user.user_name, user.email, code)
+        insertion_cmd = self.config['database']['commands']['users_table_insertion'].format(values)
+        database.execute(insertion_cmd)
+        database.close_connection()
+
+        self.logger.info(f"Referral code generated for {user.user_name}: {code}")
+        return code
